5f7a452f7834874adfb57d12252d3ebdc8b7fc7d
Kutluhan Duman <kutluduman@gmail.com>
2020-07-20 20:29:01 +0000

    Assert equal function is created

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+    console.log('✅✅✅Assertion passed');
+  } else {
+    console.log('🛑🛑🛑Assertion failed');
+  }
+};
+
+assertEqual('Lighthouse Labs','Bootcamp');
+assertEqual(1,1);
