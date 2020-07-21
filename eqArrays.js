2e9880a2840e77423f7fa3bbfe0796bfc30a4967
kutluduman <kutluduman@gmail.com>
2020-07-21 18:50:08 -0400

    A function is created in order to check whether two arrays are equal or not

---
+++ eqArrays.js
@@ -0,0 +1,40 @@
+
+let success = "✅✅✅Assertion passed";
+let failure = "🛑🛑🛑Assertion failed";
+
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+    console.log(`${success}`);
+  } else {
+    console.log(`${failure}`);
+  }
+};
+
+
+const eqArrays = (actualArray,expectedArray) => {
+
+ let actualOutput = "";
+ let expectedOutput = "";
+
+ for(let i = 0 ; i<actualArray.length ; i++) {
+
+    actualOutput+=actualArray[i];
+
+ }
+
+ for(let j = 0 ; j<expectedArray.length ; j++){
+
+    expectedOutput+= expectedArray[j];
+
+ }
+
+ if(actualOutput===expectedOutput) {
+   return true;
+ }
+ return false;
+
+
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true);
