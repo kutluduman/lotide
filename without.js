71763e1a6f9f701758c6fc9638599a2459b3e260
kutluduman <kutluduman@gmail.com>
2020-07-21 20:30:37 -0400

    Created a function that prints out the array by removing the unwanted values, which comes from the parameter

---
+++ without.js
@@ -0,0 +1,68 @@
+const eqArrays = (actualArray,expectedArray) => {
+
+  let actualOutput = "";
+  let expectedOutput = "";
+ 
+  for(let i = 0 ; i < actualArray.length ; i++) {
+ 
+     actualOutput += actualArray[i];
+ 
+  }
+ 
+  for(let j = 0 ; j < expectedArray.length ; j++){
+ 
+     expectedOutput += expectedArray[j];
+ 
+  }
+ 
+  if(actualOutput === expectedOutput) {
+    return true;
+  }
+  return false;
+ 
+ 
+ }
+ 
+ const assertArraysEqual = (value) => {
+
+  if(value) {
+
+      console.log("Arrays are equal"); 
+
+  } else {
+
+    console.log("Arrays are not equal");
+  }
+
+ }
+
+
+ const without = (source, itemsToRemove) => {
+  
+
+  for(let i = 0 ; i < source.length ; i++) {
+        
+     for(let j = 0 ; j < itemsToRemove.length ; j++) {
+
+        if(source[i] === itemsToRemove[j]) {
+
+          source.splice(i,1);
+
+        }
+
+     }
+      
+  }
+
+    return source ;
+  
+
+ }
+
+ assertArraysEqual((without([1, 2, 3], [1])), [2, 3]);
+ assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])),["1", "2"]);
+
+ const words = ["hello", "world", "lighthouse"];
+ without(words, ["lighthouse"]); // no need to capture return value for this test case
+ // Make sure the original array was not altered by the without function
+ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
